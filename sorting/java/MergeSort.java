acdb76d17c48b1e7158327fee30b50588d58d0ef
Anshuman <anshumannaikgodfather@gmail.com>
2025-08-17 21:31:00 +0530

    Added MergeSort

--- sorting/java/MergeSort.java
+++ sorting/java/MergeSort.java
@@ -1,5 +1,23 @@
 package sorting.java;
 
-public class MergeSort {
+import java.util.Arrays;
+import arrays.java.MergeTwoSortedArrays;
 
+public class MergeSort {
+    public static int[] mergeSort(int[] arr) { // using recursion
+
+        // base condition
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        // get the middle index
+        int mid = arr.length / 2;
+
+        // creating new objects
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return MergeTwoSortedArrays.merge(left, right);
+    }
 }
